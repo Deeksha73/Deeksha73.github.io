3eb5d508b2c3e60c741f5e23bb814bc73b36dbfd
Deeksha73 <deekshasingh945@gmail.com>
2018-08-05 12:47:47 +0530

    api

---
+++ controllers/profile.js
@@ -0,0 +1,14 @@
+const handleProfileGet =(req,res,knex)=>{
+	const {id} = req.params;
+	knex.select('*').from('users').where({id})
+	.then(user=>{
+		if(user.length)
+			res.json(user[0])
+		else
+			res.status(400).json('not found')
+	})
+	.catch(err=> res.json('error'))
+}
+module.exports={
+	handleProfileGet: handleProfileGet
+}
